239abd5a10d8be0d797b183b1193cbfd08672d6b
emanuelpras-dev <emanuelpras.dev@gmail.com>
2022-09-15 22:17:08 +0700

    services - provide data product on array

---
+++ shopping-cart-blazor/Services/ProductProviderService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Immutable;
+using shopping_cart_blazor.Models;
+
+namespace shopping_cart_blazor.Services
+{
+    public static class ProductProviderService
+    {
+        public static readonly ImmutableList<Product> Products = ImmutableList.CreateRange(new List<Product>()
+    {
+        new()
+        {
+            Id = Guid.NewGuid(),
+            Name = "All roads lead to Blazor Server",
+            Description = "A tutorial book for Blazor Server technology",
+            Price = 70
+        },
+        new()
+        {
+            Id = Guid.NewGuid(),
+            Name = "All roads lead to Blazor WASM",
+            Description = "A tutorial book for Blazor WASM technology.",
+            Price = 70
+        },
+        new()
+        {
+            Id = Guid.NewGuid(),
+            Name = "gRPC for Blazor WASM",
+            Description = "A tutorial book for developing a backend for Blazor WASM technology.",
+            Price = 100
+        }
+    });
+    }
+}
+
